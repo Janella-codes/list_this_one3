9af84355b59950a4971f9537b5399ac4185ee762
Janella-codes <118861074+Janella-codes@users.noreply.github.com>
2023-12-15 20:37:23 -0800

    Add auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,36 @@
+import { TodoItem } from "@/components/TodoItem";
+import { prisma } from "./db";
+import Link from "next/link";
 
+function getTodos() {
+  return prisma.todo.findMany()
+}
 
+async function toggleTodo(id: string, complete: boolean) {
+  "use server"
 
+  await prisma.todo.update( {where: { id }, data: { complete } })
+}
 
+export default async function Home() {
+  const todos = await prisma.todo.findMany()
 
-
-export default function Home() {
-  return (
-
-    <div>
-      <h1>Home this one </h1>
-    </div>
-  );
+  return ( <>
+    <header className="flex justify-between items-center mb-4">
+      <h1 className="text-2xl">To dos</h1>
+      <Link 
+        className="border border-slate-300 text-slate-800 px-2 py-1 rounded
+        hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        href="/new"
+        >
+          New
+        </Link>
+    </header>
+    <ul className="p1-4">
+      {todos.map(todo => (
+        <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+      ))}
+    </ul>
+  </>
+  )
 }
